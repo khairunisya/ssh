bdc516672ef9c774b5561b9a4dced5841aa2d4c1
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-27 01:15:33 +0700

    up

---
+++ addssh.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+bersih
+MYIP=$(wget -qO- ipinfo.io/ip);
+
+clear
+read -p "Username : " Login
+read -p "Password : " Pass
+read -p "Expired (hari): " masaaktif
+
+
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /home/domain)
+else
+domain=$IP
+fi
+wstls="$(cat ~/log-install.txt | grep -w "WebSocket TLS" | cut -d: -f2)"
+wsdrop="$(cat ~/log-install.txt | grep -w "WebSocket Dropbear" | cut -d: -f2)"
+wsssh="$(cat ~/log-install.txt | grep -w "WebSocket OpenSSH" | cut -d: -f2)"
+wsovpn="$(cat ~/log-install.txt | grep -w "WebSocket OpenVPN" | cut -d: -f2)"
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
+sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+drop="$(cat ~/log-install.txt | grep -w " Dropbear" | cut -d: -f2)"
+ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+cat > /etc/port-$Login.json <<END
+{
+      "openssh": "22",
+      "dropbear": "${drop}",
+      "stunnel": "${ssl}",
+      "wstls": "${wstls}",
+      "wsdrop": "${wsdrop}",
+      "wsssh": "${wsssh}",
+      "wsovpn": "${wsovpn}",
+      "squid": "${sqd}"
+ }
+END
+clear
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "Terima Kasih Telah Menggunakan Layanan Kami"
+echo -e "Berikut Detail Akun SSH Dan OpenVPN"
+echo -e "==============================="
+echo -e "Username       : $Login "
+echo -e "Password       : $Pass"
+echo -e "Domain         : $domain"
+echo -e "==============================="
+echo -e "Host           : $MYIP"
+echo -e "OpenSSH        : 22"
+echo -e "Dropbear       :$drop"
+echo -e "Stunnel        :$ssl"
+echo -e "WS SSL         :$wstls"
+echo -e "WS Dropbear    :$wsdrop"
+echo -e "WS OpenSSH     :$wsssh"
+echo -e "WS OVPN        :$wsovpn"
+echo -e "Port Squid     :$sqd"
+echo -e "OpenVPN        : TCP $ovpn http://$MYIP:81/client-tcp-$ovpn.ovpn"
+echo -e "OpenVPN        : UDP $ovpn2 http://$MYIP:81/client-udp-$ovpn2.ovpn"
+echo -e "OpenVPN        : SSL 442 http://$MYIP:81/client-tcp-ssl.ovpn"
+echo -e "badvpn         : 7100-7300"
+echo -e "==============================="
+echo -e "Payload Websocket :"
+echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "===============================" |
+echo -e "Setting SSH Websocket :"
+echo -e "bimbel.ruangguru.com:2082@$Login:$Pass"
+echo -e "==============================="
+echo -e "Aktif Selama   : $masaaktif Hari"
+echo -e "Berakhir Pada  : $exp"
+echo -e "Mod By Jrtunnel"
---
+++ setup.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
+apt update
+
+apt install -y bzip2 gzip coreutils screen curl
+apt install python -y
+mkdir /var/lib/premium-script;
+
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
+
+wget https://raw.githubusercontent.com/khairunisya/ssh/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+
+wget https://raw.githubusercontent.com/khairunisya/ssh/main/websock.sh && chmod +x websock.sh && ./websock.sh
+
+rm -f ssh-vpn.sh
+rm -f websock.sh
+
+cat > /usr/bin/bersih << END
+#!/bin/bash
+echo 1 > /proc/sys/vm/drop_caches
+echo 2 > /proc/sys/vm/drop_caches
+echo 3 > /proc/sys/vm/drop_caches
+swapoff -a
+swapon -a
+ban
+clear-log
+END
+chmod +x /usr/bin/bersih
+
+cat <<EOF> /etc/systemd/system/autosett.service
+
+[Unit]
+
+Description=autosetting
+
+Documentation=https://www.jrtunnel.com
+
+[Service]
+
+Type=oneshot
+
+ExecStart=/bin/bash /etc/set.sh
+
+RemainAfterExit=yes
+
+[Install]
+
+WantedBy=multi-user.target
+
+EOF
+
+systemctl daemon-reload
+
+systemctl enable autosett
+
+wget -O /etc/set.sh "https://raw.githubusercontent.com/Afdhan/sc/main/set.sh"
+
+chmod +x /etc/set.sh
+
+echo "1.1.1" > /home/ver
+
+wget -O /usr/bin/addssh https://raw.githubusercontent.com/khairunisya/ssh/main/addssh.sh && chmod +x /usr/bin/addssh
+
+clear
+
+figlet -f slant Jrtunnel - Fastnet | lolcat
+
+echo " "
+
+echo " "
+
+echo "=================================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
+
+echo "" | tee -a log-install.txt
+
+echo "   >>> Service Port" | tee -a log-install.txt
+
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
+
+echo "   - Stunnel4                : 443, 171"  | tee -a log-install.txt
+
+echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
+
+echo "   - WebSocket Dropbear      : 8880"  | tee -a log-install.txt
+
+echo "   - WebSocket OpenSSH       : 2095"  | tee -a log-install.txt
+
+echo "   - WebSocket OpenVPN       : 2082"  | tee -a log-install.txt
+
+echo "   - WebSocket TLS           : 443"  | tee -a log-install.txt
+
+echo "   - SSLH Multiplexer        : 443"  | tee -a log-install.txt
+
+echo "   - Squid Proxy             : 3128, 8080 (limit to IP SSH)"  | tee -a log-install.txt
+
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+
+echo "   - Wireguard               : 7070"  | tee -a log-install.txt
+
+echo ""  | tee -a log-install.txt
+
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+
+echo "   - Autoreboot On 00.00-12.00 GMT +7" | tee -a log-install.txt
+
+echo "   - Autobackup Data" | tee -a log-install.txt
+
+echo "   - Restore Data" | tee -a log-install.txt
+
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+
+echo "   - White Label" | tee -a log-install.txt
+
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+
+echo ""  | tee -a log-install.txt
+
+echo "=================================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
+
+echo "- Mod By M AFDHAN & NezaVPN" | tee -a log-install.txt
+
+echo ""
+
+sleep 1
+
+  echo -e "Setup Install Sukses!" | lolcat
+
+rm -f setup.sh
+sleep 5
+reboot
