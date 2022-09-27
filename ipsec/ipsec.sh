6bef659c650cd32d59abf9f4edcc257932e26e0e
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-27 08:59:21 +0700

    up

--- ipsec/ipsec.sh
+++ ipsec/ipsec.sh
@@ -31,6 +31,9 @@ fi
 jrtunnel="raw.githubusercontent.com/khairunisya/ssh/main/ipsec"
 
 VPN_IPSEC_PSK='jrtunnel.com'
+VPN_USER='mamang'
+VPN_PASSWORD='mamang'
+
 NET_IFACE=$(ip -o $NET_IFACE -4 route show to default | awk '{print $5}');
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 source /etc/os-release
@@ -273,7 +276,7 @@ bigecho "Updating IPTables rules..."
 service fail2ban stop >/dev/null 2>&1
 iptables -t nat -I POSTROUTING -s 10.10.10.0/24 -o $NET_IFACE -j MASQUERADE
 iptables -t nat -I POSTROUTING -s 192.168.43.0/24 -o $NET_IFACE -j MASQUERADE
-iptables --table nat --append POSTROUTING --jump MASQUERADE
+iptables -t nat -I POSTROUTING -s 192.168.41.0/24 -o $NET_IFACE -j MASQUERADE
 if [[ ${OS} == "centos" ]]; then
 service iptables save
 iptables-restore < /etc/sysconfig/iptables 
