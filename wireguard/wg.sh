568cbd8b56a8a5bf15a663b1f0b6c815d77cc02e
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-28 22:47:22 +0700

    up

--- wireguard/wg.sh
+++ wireguard/wg.sh
@@ -56,12 +56,12 @@ SERVER_PUB_NIC=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 	if [[ $OS == 'ubuntu' ]]; then
 	apt install -y wireguard
 elif [[ $OS == 'debian' ]]; then
-	#echo "deb http://ftp.debian.org/debian buster-backports main" >/etc/apt/sources.list.d/backports.list
-	echo "deb http://deb.debian.org/debian/ unstable main" >/etc/apt/sources.list.d/unstable.list
-	printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' >/etc/apt/preferences.d/limit-unstable
+	echo "deb http://ftp.debian.org/debian buster-backports main" >/etc/apt/sources.list.d/backports.list
+	#echo "deb http://deb.debian.org/debian/ unstable main" >/etc/apt/sources.list.d/unstable.list
+	#printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' >/etc/apt/preferences.d/limit-unstable
 	apt-get update
 	#apt-get install -y iptables resolvconf qrencode
-    #apt-get install -y -t buster-backports wireguard
+    apt-get install -y -t buster-backports wireguard
 	apt install -y wireguard-tools iptables iptables-persistent
 	apt install -y linux-headers-$(uname -r)
 elif [[ ${OS} == 'centos' ]]; then
