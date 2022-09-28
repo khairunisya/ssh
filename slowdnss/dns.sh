62c0d32ae77ff0b3d9fc4e23a22d28794ec61caa
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-29 02:00:43 +0700

    Update dns.sh

--- slowdnss/dns.sh
+++ slowdnss/dns.sh
@@ -8,8 +8,8 @@ apt install jq curl -y
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
 DOMAIN=jrtunnel.us
-SUB_DOMAIN=sg6dns.jrtunnel.us
-NS_DOMAIN=ns-sg6dns.jrtunnel.us
+SUB_DOMAIN=usdns.jrtunnel.us
+NS_DOMAIN=ns-usdns.jrtunnel.us
 CF_ID=vstunnel@gmail.com
 CF_KEY=bf2f943aba9cefaf4cc246ab198519ab15e93
 
