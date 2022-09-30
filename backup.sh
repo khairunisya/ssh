2bd342f8e3fad386c65f04cfd6806c4badabb350
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-10-01 05:42:39 +0700

    up

---
+++ backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Jrtunnel
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+clear
+IP=$(wget -qO- ipinfo.io/ip);
+date=$(date +"%Y-%m-%d")
+clear
+echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
+rm -rf /root/backup
+mkdir /root/backup
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+cd /root
+zip -r $IP-$date.zip backup > /dev/null 2>&1
+
+echo "Simpan Zip Backup Di Root Folder"
